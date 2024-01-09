11e02dbd1cbe16a5dcaca2bdaf5d00d4768c9d24
Lucas Saud <90074068+lucassaud@users.noreply.github.com>
2024-01-09 13:24:32 -0300

    Update index.js

--- frontend/src/pages/Financeiro/index.js
+++ frontend/src/pages/Financeiro/index.js
@@ -149,12 +149,12 @@ const Invoices = () => {
     var dias = moment.duration(diff).asDays();    
     const status = record.status;
     if (status === "paid") {
-      return "Pago";
+      return "Pagado";
     }
     if (dias < 0) {
       return "Vencido";
     } else {
-      return "Em Aberto"
+      return "Pendiente";
     }
 
   }
@@ -170,7 +170,7 @@ const Invoices = () => {
 
       ></SubscriptionModal>
       <MainHeader>
-        <Title>Faturas</Title>
+        <Title>Facturas</Title>
       </MainHeader>
       <Paper
         className={classes.mainPaper}
@@ -181,11 +181,11 @@ const Invoices = () => {
           <TableHead>
             <TableRow>
               <TableCell align="center">Id</TableCell>
-              <TableCell align="center">Detalhes</TableCell>
+              <TableCell align="center">Detalles</TableCell>
               <TableCell align="center">Valor</TableCell>
-              <TableCell align="center">Data Venc.</TableCell>
-              <TableCell align="center">Status</TableCell>
-              <TableCell align="center">Ação</TableCell>
+              <TableCell align="center">Fecha de Vencimiento</TableCell>
+              <TableCell align="center">Estado</TableCell>
+              <TableCell align="center">Acción</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>

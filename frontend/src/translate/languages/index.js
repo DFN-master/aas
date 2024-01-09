0e178ff9320d2d13c38df81c57fc76163bc39404
Lucas Saud <90074068+lucassaud@users.noreply.github.com>
2024-01-09 12:49:35 -0300

    Update index.js

--- frontend/src/translate/languages/index.js
+++ frontend/src/translate/languages/index.js
@@ -1,10 +1,6 @@
-import { messages as portugueseMessages } from "./pt";
-import { messages as englishMessages } from "./en";
 import { messages as spanishMessages } from "./es";
 
 const messages = {
-	...portugueseMessages,
-	...englishMessages,
 	...spanishMessages,
 };
 

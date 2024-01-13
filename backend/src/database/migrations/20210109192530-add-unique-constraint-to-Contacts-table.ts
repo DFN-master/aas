20c31470bde48c8b287972dbf7226a637299f9c0
Lucas Saud <90074068+lucassaud@users.noreply.github.com>
2024-01-13 07:26:22 -0300

    fixes

--- backend/src/database/migrations/20210109192530-add-unique-constraint-to-Contacts-table.ts
+++ backend/src/database/migrations/20210109192530-add-unique-constraint-to-Contacts-table.ts
@@ -1,17 +1,27 @@
-import { QueryInterface } from "sequelize";
 
-module.exports = {
-  up: (queryInterface: QueryInterface) => {
-    return queryInterface.addConstraint("Contacts", ["number", "companyId"], {
-      type: "unique",
-      name: "number_companyid_unique"
-    });
-  },
+//module.exports = {
+  //up: (queryInterface: QueryInterface) => {
+  //  return queryInterface.addConstraint("Contacts", ["number", "companyId"], {
+  //    type: "unique",
+  //    name: "number_companyid_unique"
+  //  });
+  //},
+  import { QueryInterface, DataTypes, Sequelize } from "sequelize";
 
-  down: (queryInterface: QueryInterface) => {
-    return queryInterface.removeConstraint(
-      "Contacts",
-      "number_companyid_unique"
-    );
-  }
-};
+  module.exports = {
+    up: (queryInterface: QueryInterface) => {
+      return queryInterface.addConstraint("Contacts", {
+        type: 'UNIQUE',
+        fields: ["number", "companyId"],
+        name: 'number_companyid_unique',
+      });
+    },
+
+    down: (queryInterface: QueryInterface) => {
+      return queryInterface.removeConstraint(
+        "Contacts",
+        "number_companyid_unique"
+      );
+    }
+  };
+
